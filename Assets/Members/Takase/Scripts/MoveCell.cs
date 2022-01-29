1f07b5f126ab4f1e6ad8a5dee9103b4ff045a423
TomohitoTakase <takase3962101@gmail.com>
2022-01-29 12:17:39 +0900

    [add]Playerのクラス作成

---
+++ Assets/Members/Takase/Scripts/MoveCell.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class MoveCell : MonoBehaviour
+{
+    [SerializeField] private Player _Player;
+
+    [SerializeField] private float _moveTargetY;
+    public float MoveTargetY => _moveTargetY;
+
+    public void OnClick()
+    {
+        _Player.Move(_moveTargetY);
+    }
+}
---
+++ Assets/Members/Takase/Scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(Animator))]
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Animator _Animator;
+
+    private Tween _tweener;
+
+    private void Reset()
+    {
+        _Animator = GetComponent<Animator>();
+    }
+
+    public void Move(float moveTargetY)
+    {
+        // 移動中なので、何もさせない
+        if (_tweener != null)
+        {
+            return;
+        }
+
+        _tweener = transform.DOMoveY(moveTargetY, 1.0f);
+        _tweener.onComplete = () => { _tweener = null; };
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (IsCheckForcedScrollGameOver(collision))
+        {
+            // 強制スクロールでGameOverになる演出があれば
+
+            SendGameOver();
+            return;
+        }
+        else if (IsCheckDropGameOver(collision))
+        {
+            // ダメなマスでGameOverになる演出があれば
+
+            SendGameOver();
+            return;
+        }
+
+        // スコアアップ処理
+    }
+
+    private bool IsCheckForcedScrollGameOver(Collider2D collision)
+    {
+        // とりあえず仮置き
+        return collision.gameObject.tag == "ScrollWall";
+    }
+
+    private bool IsCheckDropGameOver(Collider2D collision)
+    {
+        // とりあえず仮置き
+        return collision.gameObject.tag == "Drop";
+    }
+
+    private void SendGameOver()
+    {
+        // GameManagerにゲームが終了したことを通知
+    }
+
+}
