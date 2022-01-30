e6aa618adebad6e25bf9baaf967ecee10c477461
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-01-30 11:51:13 +0900

    タイトルのイメージ等を変更しました

--- Assets/Members/Takai/Scripts/TitleScripts.cs
+++ Assets/Members/Takai/Scripts/TitleScripts.cs
@@ -11,68 +11,189 @@ public class TitleScripts : MonoBehaviour
     [SerializeField] GameObject _mainCanvas;
 
     [Header("スタートボタン")]
-    [SerializeField] Button _startButton;
+    [SerializeField] GameObject _startButton;
     [SerializeField] string _inGameName = ""; //スタートした後に飛ぶシーン名を入れる
+    [SerializeField] float _fadeTime;
 
     [Header("Audioボタン")]
     [SerializeField] Button _audioButton;
     [SerializeField] GameObject _audioCanvas;
+    [SerializeField] GameObject _backLight1;
 
     [Header("やり方説明ボタン")]
     [SerializeField] Button _ruleButton;
     [SerializeField] GameObject _ruleCanvas;
+    [SerializeField] GameObject _backLight2;
 
     [Header("ランキングボタン")]
     [SerializeField] Button _runkButton;
     [SerializeField] GameObject _rankCanvas;
+    [SerializeField] GameObject _backLight3;
 
     [Header("もどるボタン")]
-    [SerializeField] Button _backButton;
+    [SerializeField] Button _quitButton;
+    [SerializeField] GameObject _backLight4;
 
     [Header("フェードイメージ")]
-    [SerializeField] Image _fadeImage;
+    [SerializeField] Image _fadeImageWhite;
+    [SerializeField] Image _fadeImageBlack;
+
+    int _selectButton = -1;
 
     void Start()
     {
-        _fadeImage.gameObject.SetActive(false);
+        _fadeImageWhite.gameObject.SetActive(false);
+        _fadeImageBlack.gameObject.SetActive(false);
     }
     public void OnClickStart()
     {
         DOFadeLoadScene(1f);
     }
 
-    public void OnClickAudio()
+    public void OnClickAudio(int num)
     {
-        _audioCanvas.SetActive(true);
+        if (_selectButton != num)
+        {
+            _backLight1.SetActive(true);
+            _backLight2.SetActive(true);
+            _backLight3.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioCanvas.SetActive(true);
+            _rankCanvas.SetActive(false);
+            _ruleCanvas.SetActive(false);
+            _startButton.SetActive(false);
+
+            _backLight2.SetActive(false);
+            _backLight3.SetActive(false);
+            _backLight4.SetActive(false);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+
+            _selectButton = num;
+        }
+        else
+        {
+            _audioCanvas.SetActive(false);
+            _rankCanvas.SetActive(false);
+            _ruleCanvas.SetActive(false);
+            _startButton.SetActive(true);
+
+            _backLight2.SetActive(true);
+            _backLight3.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+            _selectButton = -1;
+        }
     }
 
-    public void OnClickRank()
+    public void OnClickRank(int num)
     {
-        _rankCanvas.SetActive(true);
-        
+        if (_selectButton != num)
+        {
+            _backLight1.SetActive(true);
+            _backLight2.SetActive(true);
+            _backLight3.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioCanvas.SetActive(false);
+            _rankCanvas.SetActive(true);
+            _ruleCanvas.SetActive(false);
+            _startButton.SetActive(false);
+
+            _backLight1.SetActive(false);
+            _backLight2.SetActive(false);
+            _backLight4.SetActive(false);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+
+            _selectButton = num;
+        }
+        else
+        {
+            _audioCanvas.SetActive(false);
+            _rankCanvas.SetActive(false);
+            _ruleCanvas.SetActive(false);
+            _startButton.SetActive(true);
+
+            _backLight1.SetActive(true);
+            _backLight2.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+            _selectButton = -1;
+        }
     }
 
-    public void OnClickRule()
+    public void OnClickRule(int num)
     {
-        _ruleCanvas.SetActive(true);
+        if (_selectButton != num)
+        {
+            _backLight1.SetActive(true);
+            _backLight2.SetActive(true);
+            _backLight3.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioCanvas.SetActive(false);
+            _rankCanvas.SetActive(false);
+            _ruleCanvas.SetActive(true);
+            _startButton.SetActive(false);
+
+            _backLight1.SetActive(false);
+            _backLight3.SetActive(false);
+            _backLight4.SetActive(false);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 100);
+
+            _selectButton = num;
+        }
+        else
+        {
+            _audioCanvas.SetActive(false);
+            _rankCanvas.SetActive(false);
+            _ruleCanvas.SetActive(false);
+            _startButton.SetActive(true);
+
+            _backLight1.SetActive(true);
+            _backLight3.SetActive(true);
+            _backLight4.SetActive(true);
+
+            _audioButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _runkButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _ruleButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            _quitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+
+            _selectButton = -1;
+        }
     }
 
-    public void OnClickBack()
-    {
-        _audioCanvas.SetActive(false);
-        _ruleCanvas.SetActive(false);
-        _rankCanvas.SetActive(false);
-    }
-
-    public void OnClickQuit()
+    public void OnClickQuit(int num)
     {
         DoFadeImageOut(1f);
     }
 
     public void DoFadeImageOut(float color)
     {
-        _fadeImage.gameObject.SetActive(true);
-        _fadeImage.DOFade(color, 3f).OnComplete(() =>
+        _fadeImageBlack.gameObject.SetActive(true);
+        _fadeImageBlack.DOFade(color, 3f).OnComplete(() =>
         {
 #if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
@@ -84,7 +205,7 @@ public class TitleScripts : MonoBehaviour
 
     public void DOFadeLoadScene(float color)
     {
-        _fadeImage.gameObject.SetActive(true);
-        _fadeImage.DOFade(color, 3f).OnComplete(() => SceneManager.LoadScene(_inGameName));
+        _fadeImageWhite.gameObject.SetActive(true);
+        _fadeImageWhite.DOFillAmount(color, _fadeTime).SetEase(Ease.OutQuint).OnComplete(() => SceneManager.LoadScene(_inGameName));
     }
 }
